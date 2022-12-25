dbe1db5e12dcbccdc8b9b8e0526bf737142a7cb3
MXCAZ <final00717@hotmail.com>
2022-12-25 15:50:01 -0700

    .

--- models/user.js
+++ models/user.js
@@ -3,19 +3,23 @@ const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 
 class User extends Model {
-  checkPassword(login) {
-    return bcrypt.compareSync(login, this.password);
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
+// define table columns and configuration
 User.init(
   {
+    // define an id column
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    // define a username column
     username: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -23,8 +27,9 @@ User.init(
 
     github: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
+    // define an email column
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -33,6 +38,7 @@ User.init(
         isEmail: true,
       },
     },
+    // define a password column
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -41,13 +47,14 @@ User.init(
       },
     },
   },
-
   {
     hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+      // set up beforeUpdate lifecycle "hook" functionality
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(
           updatedUserData.password,
@@ -61,7 +68,7 @@ User.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    moduleName: "user",
+    modelName: "user",
   }
 );
 
