83c7a0801fe71e36de257db92654a8837f3b35b2
MXCAZ <final00717@hotmail.com>
2022-12-25 10:58:52 -0700

    .

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,6 @@ const sequelize = process.env.JAWSDB_URL
       {
         host: "127.0.0.1",
         dialect: "mysql",
-        port: "3306",
 
         dialectOptions: {
           decimalNumbers: true,
